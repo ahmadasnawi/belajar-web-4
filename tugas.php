f662f1f0d8922e46fad8f1a0e594dd650ab63c7b
ahmadasnawi <ahmadaries899@gmail.com>
2024-08-04 09:20:37 +0700

    first commit

---
+++ tugas.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <div id="container">
+            <div class="kotak-atas">
+                <h1>Registration</h1>
+                <div class="garis"></div>
+            </div>
+            <div class="kotak-tengah">
+                <div class="box-left">
+                    <div class="keterangan">
+                        <label for="aa">Full Name</label>
+                        <input type="text" placeholder="Enter your name">
+                    </div>
+                    <div class="keterangan">
+                        <label for="">Email</label>
+                        <input type="text" placeholder="Enter your email">
+                    </div>
+                    <div class="keterangan">
+                        <label for="">Password</label>
+                        <input type="text" placeholder="Enter your pasword">
+                    </div>
+                </div>
+                <div class="box-right">
+                    <div class="keterangan">
+                        <label for="">Username</label>
+                        <input type="text" placeholder="Enter your username">
+                    </div>
+                    <div class="keterangan">
+                        <label for="">Phone Number</label>
+                        <input type="text" placeholder="Enter your number">
+                    </div>
+                    <div class="keterangan">
+                        <label for="">Confirm Pasword</label>
+                        <input type="text" placeholder="Confirm your pasword">
+                    </div>
+                </div>
+            </div>
+            <div class="kotak-tengah2">
+                <h2>Gender</h2>
+                <div class="radio">
+                    <div class="radio1">
+                        <input type="radio">
+                        <label for="">male</label>
+                    </div>
+                    <div class="radio1">
+                        <input type="radio">
+                        <label for="">Fermale</label>
+                    </div>
+                    <div class="radio1">
+                        <input type="radio">
+                        <label for="">Prefer not to say</label>
+                    </div>
+                </div>
+            </div>
+                <div class="kotak-bawah">
+                    <button>Register</button>
+                </div>
+        </div>
+    </section>
+</body>
+</html>
